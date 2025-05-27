66db8fefe8cba044207108c385bcc30d37e9897b
Shaira Causil <causil845@gmail.com>
2025-05-27 09:12:37 -0500

    Update VentanaPrincipal.java

--- MVC/vista/VentanaPrincipal.java
+++ MVC/vista/VentanaPrincipal.java
@@ -33,7 +33,7 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
      */
     public VentanaPrincipal() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 453, 327);
+        setBounds(100, 100, 500, 500);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
